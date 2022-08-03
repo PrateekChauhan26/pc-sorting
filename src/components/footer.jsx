4b6d7dbc439f1003bfe009228c62af9f9faf83be
Prateek <prateekchauhanhello@gmail.com>
2022-08-03 14:40:43 +0530

    final commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
+                <a href="https://www.linkedin.com/in/prateek-chauhan-28442199/" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                    Prateek Chauhan
+                </a>
+            </p>
+        </footer>
+    );
+}
+export default Footer;
